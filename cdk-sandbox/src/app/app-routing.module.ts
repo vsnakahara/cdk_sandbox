14865ef23cd9aa0cc41b4b8cee199065c7f0e4e2
Vanessa Nakahara <vsnakahara@gmail.com>
2021-02-01 20:40:13 -0300

    Adicionado componente de teste

--- cdk-sandbox/src/app/app-routing.module.ts
+++ cdk-sandbox/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'list', loadChildren: () => import('./list/list.module').then(m => m.ListModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
