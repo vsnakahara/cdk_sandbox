a7fcb22707d6d6eb8c94df739aab4dc56b9f4865
vinicius.grott <vinicius.grott@accountfy.com>
2021-02-06 13:19:02 -0300

    Upgrade para angular 11

--- cdk-sandbox/src/app/app-routing.module.ts
+++ cdk-sandbox/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [{ path: 'list', loadChildren: () => import('./list/list.module').then(m => m.ListModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
