8f9f849b8f6790d391d8b5b6f7f7979121b5fea2
vinicius.grott <vinicius.grott@accountfy.com>
2021-02-07 19:45:57 -0300

    Testes com grid-list

--- cdk-sandbox/src/app/components/commerce/commerce.component.ts
+++ cdk-sandbox/src/app/components/commerce/commerce.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-commerce',
-  templateUrl: './commerce.component.html',
-  styleUrls: ['./commerce.component.scss']
+  selector: "app-commerce",
+  templateUrl: "./commerce.component.html",
+  styleUrls: ["./commerce.component.scss"],
 })
 export class CommerceComponent implements OnInit {
+  items = [
+    {
+      name: "Shiba Ino seminovo",
+      img: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+    },
+    {
+      name: "Shiba Ino seminovo",
+      img: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+    },
+    {
+      name: "Shiba Ino seminovo",
+      img: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+    },
+    {
+      name: "Shiba Ino seminovo",
+      img: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+    },
+    {
+      name: "Shiba Ino seminovo",
+      img: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+    },
+    {
+      name: "Shiba Ino seminovo",
+      img: "https://material.angular.io/assets/img/examples/shiba2.jpg",
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
